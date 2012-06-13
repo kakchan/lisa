38c6de5245ae1f7301d67646d5d0bc59e8685c16
Ben Birch <bbirch@aconex.com>
2012-06-13 19:18:25 +1000

    initial curl.js support

--- src/app.js
+++ src/app.js
@@ -1,33 +1,37 @@
-define( [ "lib/polyfills", "lib/jquery", "lib/jquery-nohtml", "ui/stack/stack", "ui/card/card", "ui/button/button" ], function( p, $, nohtml, Stack, Card, Button ) {
+define( [ "ui/stack/stack", "ui/card/card", "ui/button/button", "ui/abstractwidget/abstractwidget" ], function( Stack, Card, Button, AbstractWidget ) {
 		
 	loadCss("src/app.css");
 	
-	var app = new Stack({ parent: "body"	});
+	return AbstractWidget.extend({
+		init: function() {
+			var stack = new Stack({ parent: "body"	});
 
-	var transitions = [ "slide", "fade", "flip", "zoom" ];
+			var transitions = [ "slide", "fade", "flip", "zoom" ];
 
-	var ids = "a,b,c,d,e,f,g".split(",");
-	var pages = ids.map( function( id, index ) {
-		return new Card({
-			id: id,
-			title: "Page " + id.toUpperCase(),
-			children: [
-				{ tag: "H1", text: "Page " + id.toUpperCase() },
-				new Button({
-					label: "<- Prev",
-					onclick: function() {
-						app.pop();
-					}
-				}),
-				new Button({
-					label: "Next ->",
-					onclick: function() {
-						app.push( pages[ (index + 1) % ids.length ], { method: transitions[ index % transitions.length ] } );
-					}
-				})
-			]
-		});
+			var ids = "a,b,c,d,e,f,g".split(",");
+			var pages = ids.map( function( id, index ) {
+				return new Card({
+					id: id,
+					title: "Page " + id.toUpperCase(),
+					children: [
+						{ tag: "H1", text: "Page " + id.toUpperCase() },
+						new Button({
+							label: "<- Prev",
+							onclick: function() {
+								stack.pop();
+							}
+						}),
+						new Button({
+							label: "Next ->",
+							onclick: function() {
+								stack.push( pages[ (index + 1) % ids.length ], { method: transitions[ index % transitions.length ] } );
+							}
+						})
+					]
+				});
+			});
+			
+			stack.push( pages[0] );
+		}
 	});
-
-	app.push( pages[0] );
 });
